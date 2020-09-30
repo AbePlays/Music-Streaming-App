b3f28dcb606331fc5670f75b3ccf80ce76edbfa7
Abe <abhi.rawat456@gmail.com>
2020-09-30 11:31:31 +0530

    Created Album and Song struct

--- Music App/ContentView.swift
+++ Music App/ContentView.swift
@@ -8,6 +8,19 @@
 
 import SwiftUI
 
+struct Album : Hashable {
+    var id : UUID
+    var name : String
+    var image : String
+    var songs : [Song]
+}
+
+struct Song : Hashable {
+    var id : UUID
+    var name : String
+    var time : String
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello World")
